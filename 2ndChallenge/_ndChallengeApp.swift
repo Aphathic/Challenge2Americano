2da8989acd86e9056c7227017dc56a863607cefc
Shiroah <85611912+Aphathic@users.noreply.github.com>
2025-11-05 17:48:36 +0100

    Initial Commit

---
+++ 2ndChallenge/_ndChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  _ndChallengeApp.swift
+//  2ndChallenge
+//
+//  Created by Crescenzo Di Franco on 05/11/25.
+//
+
+import SwiftUI
+
+@main
+struct _ndChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
